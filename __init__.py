da4a6db8348c38197aa45ee4d72344bebff6308e
kenzam12 <136609054+kenzam12@users.noreply.github.com>
2024-11-21 15:23:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
   
-  @app.route('/tawarano/')
+@app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
